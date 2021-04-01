e0fa65182900805a70acb58a6b5230c87d7e38a2
username <sunho6200@gmail.com>
2021-04-01 23:24:50 +0900

    full calendar

--- component/MyCalendar.js
+++ component/MyCalendar.js
@@ -2,42 +2,45 @@
 import React, {useState, useEffect} from 'react';
 
 // import all the components we are going to use
-import {SafeAreaView, StyleSheet, View, TouchableOpacity, Text, ImageBackground} from 'react-native';
+import {SafeAreaView, StyleSheet, View, TouchableOpacity, Text, ImageBackground, FlatList} from 'react-native';
+import Icons from 'react-native-vector-icons/AntDesign';
 
 const weekDays = [
     "Sun","Mon","Tue","Wed","Thu","Fri","Sat"
 ]; 
 
-const image = { uri: "https://reactjs.org/logo-og.png" };
+
 
 const MyCalendar = () => {
   const [currentMonth, setCurrentMonth] = useState('');
+  const [currentYear, setCurrentYear] = useState('');
   const [thisWeek, setThisWeek] = useState('');
   const [thisDay, setThisDay] = useState('');
 
 
   useEffect(() => {
-    let date = new Date().getDate(); //Current Date
+    let date = new Date() //Current Date
     let month = new Date().getMonth() + 1; //Current Month
     let year = new Date().getFullYear(); //Current Year
-    let firstDay = new Date('March 1, 2021').getDay();
+    let firstDay = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
   
     
-    setCurrentMonth(year+'.'+month);
+    setCurrentMonth(month);
+    setCurrentYear(year);
 
-    let finalDay = 31;
+    const finalDay = [31,28,31,30,31,30,31,31,30,31,30,31];
     let day=1;
     let rows=[];
 
-    while(day<finalDay){
+    while(day<=finalDay[month-1]){
         for(let i=0;i<7;i++){
-                if(day==1&&i!=firstDay||day>finalDay){
+                if(day==1&&i!=firstDay||day>finalDay[month-1]){
                     rows.push(<TouchableOpacity style={styles.oneday}><Text> </Text></TouchableOpacity>);
                 }
                 else{
                     rows.push(<TouchableOpacity style={styles.oneday}>
-                     <ImageBackground source={require("./image/logo.png")} style={{width:40, height:40, borderRadius: 100}}>
-                         <Text>{day}</Text>
+                     <ImageBackground source={require("./image/food.jpg")} style={styles.photo}>
+                         <Text style={styles.mybutton}>{day}</Text>
                      </ImageBackground>
                     </TouchableOpacity>);
                     day++;
@@ -50,28 +53,126 @@ const MyCalendar = () => {
     
     }, []);
 
+    const addMonth=()=>{
+      let year=new Date().getFullYear();
+      let month =currentMonth;
+
+     if(month==12){
+       year=year+1;
+       month=1;
+       setCurrentMonth(month);
+       setCurrentYear(year);
+     }
+     else{
+      setCurrentMonth(currentMonth+1);
+      month=currentMonth;
+     }
+     
+      let firstDay = new Date(year, month, 1).getDay();
+      const finalDay = [31,28,31,30,31,30,31,31,30,31,30,31];
+      let day=1;
+      let rows=[];
+      
+  
+      while(day<=finalDay[month]){
+          for(let i=0;i<7;i++){
+                  if(day==1&&i!=firstDay||day>finalDay[month]){
+                      rows.push(<TouchableOpacity style={styles.oneday}><Text> </Text></TouchableOpacity>);
+                  }
+                  else{
+                      rows.push(<TouchableOpacity style={styles.oneday}>
+                      <ImageBackground source={require("./image/food.jpg")} style={styles.photo}>
+                          <Text style={styles.mybutton}>{day}</Text>
+                      </ImageBackground>
+                      </TouchableOpacity>);
+                      day++;
+                  }
+              }
+            
+              rows.push(<Text></Text>);
+          }
+          setThisWeek(rows);
+    };
+
+    const subMonth=()=>{
+      let year=new Date().getFullYear();
+      let month =currentMonth;
+
+      if(currentMonth==1){
+        setCurrentMonth(12);
+        month =12;
+        setCurrentYear(year-1);
+        year=year-1;
+      }
+      else{
+        setCurrentMonth(currentMonth-1);
+        month =currentMonth-1;
+      }
+     
+     
+      let firstDay = new Date(year, month-1, 1).getDay();
+      const finalDay = [31,28,31,30,31,30,31,31,30,31,30,31];
+      let day=1;
+      let rows=[];
+      
+  
+      while(day<=finalDay[month-1]){
+          for(let i=0;i<7;i++){
+                  if(day==1&&i!=firstDay||day>finalDay[month-1]){
+                      rows.push(<TouchableOpacity style={styles.oneday}><Text> </Text></TouchableOpacity>);
+                  }
+                  else{
+                      rows.push(<TouchableOpacity style={styles.oneday}>
+                      <ImageBackground source={require("./image/food.jpg")} style={styles.photo}>
+                          <Text style={styles.mybutton}>{day}</Text>
+                      </ImageBackground>
+                      </TouchableOpacity>);
+                      day++;
+                  }
+              }
+            
+              rows.push(<Text> </Text>);
+          }
+          setThisWeek(rows);
+    };
+
+    const renderItem = ({ item }) => (
+      <Text style={{width:50, height:30, alignContent:'center', alignItems:'center'}}>{item}</Text>
+    );
 
   return (
     <SafeAreaView style={{flex: 1}}>
-      <View style={styles.container}>
         <View style={styles.container}>
-          <Text style={styles.textStyle}>{currentMonth}</Text>
-        <View>
-            <View style={styles.oneweek}>
-                <TouchableOpacity style={styles.oneday}><Text style={styles.sunday}>{weekDays[0]}</Text></TouchableOpacity>
-                <TouchableOpacity style={styles.oneday}><Text>{weekDays[1]}</Text></TouchableOpacity>
-                <TouchableOpacity style={styles.oneday}><Text>{weekDays[2]}</Text></TouchableOpacity>
-                <TouchableOpacity style={styles.oneday}><Text>{weekDays[3]}</Text></TouchableOpacity>
-                <TouchableOpacity style={styles.oneday}><Text>{weekDays[4]}</Text></TouchableOpacity>
-                <TouchableOpacity style={styles.oneday}><Text>{weekDays[5]}</Text></TouchableOpacity>
-                <TouchableOpacity style={styles.oneday}><Text style={styles.saterday}>{weekDays[6]}</Text></TouchableOpacity>
-            </View>
-            <View style={styles.oneweek}>
-            <Text>{thisWeek}</Text>
-            </View>
+        <TouchableOpacity onPress={() => {
+            alert(new Date);
+          }}
+          
+        ><Text>Today</Text></TouchableOpacity>
+        <View style={{flexDirection:'row', alignItems: 'center', justifyContent: 'center', marginBottom: 10}}>
+        <TouchableOpacity onPress={() => {
+            subMonth()
+          }}
+          style={{marginRight:10}}
+        ><Text><Icons name="left" size={20} color="#000" /></Text></TouchableOpacity>
+        <Text style={styles.textStyle}>{currentYear}.{currentMonth}</Text>
+        <TouchableOpacity onPress={() => {
+            addMonth()
+          }}
+          style={{marginLeft:10}}
+        ><Text><Icons name="right" size={20} color="#000" /></Text></TouchableOpacity>
+        </View>
+        <View style={{ alignItems: 'center', justifyContent: 'center'}}>
+            <FlatList
+                data={weekDays}
+                renderItem={renderItem}  
+                keyExtractor={(_, index)=>index.toString()}
+                horizontal
+                style={styles.week}
+              />
+            <Text style={{width:355}}>{thisWeek}</Text>
+    
         </View>
         </View>
-      </View>
     </SafeAreaView>
   );
 };
@@ -80,20 +181,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
-    justifyContent: 'center',
-    padding: 10,
+    top: 50
+    
   },
   textStyle: {
     textAlign: 'center',
-    fontSize: 18,
+    fontSize: 20,
     color: 'black',
   },
   oneweek:{
-      flexDirection: 'row'
+      borderWidth:2
   },
   oneday:{
-      width:40,
-      height:40
+      width:50,
+      height:70
   },
 
   sunday:{
@@ -101,7 +202,19 @@ const styles = StyleSheet.create({
   },
   saterday:{
       color:'blue'
-  }
+  },
+  week:{
+    paddingHorizontal: 10
+  },
+  mybutton:{
+    color:'white',
+    padding:13,
+    fontWeight:'bold'
+   
+  },
+  photo:{
+    width:42, height:42, borderRadius: 42/2,overflow: "hidden", borderWidth: 0.1}
+
 });
 
 export default MyCalendar
